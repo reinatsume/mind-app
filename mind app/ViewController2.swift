04951b57a7ed77f2bf806fd3549c08529f716670
夏目怜奈 <reinanna7@icloud.com>
2015-06-22 00:00:07 +0900

    third commit

--- mind app/ViewController2.swift
+++ mind app/ViewController2.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
-class ViewController2: UIViewController, UITextFieldDelegate,UIPickerViewDelegate {
+class ViewController2: UIViewController,UITextFieldDelegate,UIPickerViewDelegate {
     
     let myTextField2: UITextField = UITextField(frame: CGRectMake(0,0,200,30))
     
    @IBOutlet var myUIPicker1: UIPickerView!
     let myValues: NSArray = ["覚える","ひらめき","調べる","便利"]
-    var mindArray:[String] = []
+    //myPicker1の項目
     
+
+    @IBOutlet var myUIPicker2: UIPickerView!
+    let myValues2: NSArray = ["まあまあ","普通","重要"]
+    //myPicker2の項目
+    
+    var mindArray:[String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,28 +32,40 @@ class ViewController2: UIViewController, UITextFieldDelegate,UIPickerViewDelegat
         myTextField2.layer.position = CGPointMake(self.view.bounds.width/2, 100)
         self.view.addSubview(myTextField2)
         
-     myUIPicker1 = UIPickerView()
-     myUIPicker1.delegate = self
-     self.view.addSubview(myUIPicker1)
+      myUIPicker1 = UIPickerView()
+      myUIPicker1.delegate = self
+      self.view.addSubview(myUIPicker1)
+        
+      myUIPicker2 = UIPickerView()
+      myUIPicker2.delegate = self
+      self.view.addSubview(myUIPicker2)
+        
+      
+      
     }
     
-    func numberOfComponentsInPickerView(pickerview: UIPickerView) -> Int {
+func numberOfComponentsInPickerView(pickerview: UIPickerView) -> Int {
         return 1
     }
     
-    func pickerView(pickerView: UIPickerView, numberOfComponent component: Int)->Int{
+    func pickerView(pickerView: UIPickerView!, numberOfComponent component: Int)->Int{
         return myValues.count
+    
+    
+    func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String! { return myValues[row] as! String
     }
     
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! { return myValues[row] as! String
+    func numberOfComponentsInPickerView2(pickerview: UIPickerView) -> Int {
+        return 1
     }
     
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        println("row:\(row)")
-        println("value:\(myValues[row])")
-        
-    }
+    func pickerView2(pickerView2: UIPickerView!, numberOfComponent component2: Int)->Int{
+        return myValues2.count
     
+    func pickerView2(pickerView2: UIPickerView!, titleForRow row2: Int, forComponent component2: Int) -> String! { return myValues2[row2] as! String
+        }
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -73,11 +91,12 @@ class ViewController2: UIViewController, UITextFieldDelegate,UIPickerViewDelegat
         return true
     }
     
-    @IBAction func changeTextColor(button: UIButton){
+    /*@IBAction func changeTextColor(button: UIButton){
         let colors = [UIColor.redColor(), UIColor.yellowColor(), UIColor.greenColor(),UIColor.blueColor()]
         myTextField2.textColor = colors[button.tag]
         
     }
+    */
     
     
     @IBAction func didTapmindButton() {
